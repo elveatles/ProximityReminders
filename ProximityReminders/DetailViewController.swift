30e5bd2422a6f572f1d10e2736f68a4607edea0a
Erik Carlson <noirelkscar@gmail.com>
2019-01-08 11:24:43 -0800

    Initial Commit

---
+++ ProximityReminders/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ProximityReminders
+//
+//  Created by Erik Carlson on 1/8/19.
+//  Copyright © 2019 Round and Rhombus. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
